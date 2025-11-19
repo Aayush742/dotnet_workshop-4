ef5cfd6f68c47a3a6a9ba305862249c21cd7aa71
munal777 <munalpoudel3@gmail.com>
2025-11-19 09:20:32 +0545

    feat: complete the task5 console app

---
+++ Task5/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Task_5 
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Enter days(Sunday-Saturday):....... ");
+            String day = Console.ReadLine().Trim().ToLower();
+
+            if (day == "friday" || day == "Saturday")
+            {
+                Console.WriteLine(DayType.Weekend);
+            }
+            else
+            {
+                Console.WriteLine(DayType.Weekday);
+            }
+
+            Console.WriteLine($"You entered: {day.ToUpper()}");
+
+            Book book1 = new Book("O to 1","Munal Poudel",100);
+            Book book2 = book1 with { title = "The lean startup", price = 1000 };
+            Console.WriteLine($"First book: Title:{book1.title}, Auhor:{book1.author}, Price: {book1.price} ");
+
+            var (title, author, price) = book2;
+
+            Console.WriteLine("\nDeconstructed record: ");
+            Console.WriteLine($"title:{title}");
+            Console.WriteLine($"author:{author}");
+            Console.WriteLine($"price:{price}");
+
+
+        }
+    }
+}
